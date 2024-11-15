0504c93307e9b3d24edf7afcd562cbe5d66aaba1
margoul1Malin <github.pydoctor@gmail.com>
2024-11-15 23:40:10 +0100

    Add files via upload
    Main Scanner

---
+++ PyNetScanner.py
@@ -0,0 +1,71 @@
+import scapy.all as scapy
+import re, optparse, sys, os, ipaddress
+
+#SCAPY WHO_HAS PACKET
+#BROADCAST ADRESS
+#SEND = srp()
+
+parser = optparse.OptionParser()
+parser.add_option('-i', '--ip', dest="ip", help="Type the IP u want to scan")
+parser.add_option('-a', '--aggressive', dest="aggressive", help="Scanning deeper and take more time")
+parser.add_option('-o', '--output', dest="output", help="Output IP Addresses & MAC Addresses in a file")
+(options, arguments) = parser.parse_args()
+
+tablist = []
+dico = {}
+
+def scan(ip):
+    arp_req = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_rb = broadcast/arp_req
+
+    if options.aggressive is None:
+        answered = scapy.srp(arp_rb, timeout=1)[0]
+    elif options.aggressive == "1":
+        answered = scapy.srp(arp_rb, timeout=3)[0]
+    else:
+        sys.exit('\n \033[91mAggressive option has to be 1 or Nothing\033[0m \n') 
+    
+    print("--------------------------------------------------------")
+    print('IP\t\t\t\t MAC Address')
+    print("--------------------------------------------------------")
+    
+    for a in answered:
+        print(a[1].psrc, "\t\t\t",a[1].hwsrc)
+        dico = {"IP": a[1].psrc, "MAC":a[1].hwsrc}
+        tablist.append(dico)
+        if options.output is None:
+            pass
+        else:
+            file = open(f'{options.output}.txt', 'w')
+            for e in tablist:
+                file.writelines(f"{str(e)}\n")
+            file.close()
+            
+    if options.output is None:
+        pass
+    else:
+        print('\n\033[0;32mFile saved at \t',os.path.abspath(f'{options.output}.txt'))
+        #print(a)
+
+if options.ip is None:
+    print('\n \033[91mPyNetScanner needs an IP to scan use --ip\033[0m \n')
+    
+    parser.print_help()
+    sys.exit()
+else:
+
+    a = re.match(
+    r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{2}", options.ip
+    )
+    b = re.match(
+    r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", options.ip
+    )
+
+    if bool(a) == True or bool(b) == True:
+        scan(options.ip)
+    else:
+        print('\n \033[91mInvalid IP Address or Range of IP Addresses')
+
+
+
